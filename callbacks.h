6c5261c81dc813e02f13bf672e4e7c28c65cef33
yosr-maker <57325844+yosr-maker@users.noreply.github.com>
2019-12-09 08:47:29 +0100

    Add files via upload

---
+++ callbacks.h
@@ -0,0 +1,99 @@
+#include <gtk/gtk.h>
+
+
+void
+on_buttongestresvol_clicked            (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttongestreshot_clicked            (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonretourreservation_clicked     (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonafficherlistvol_clicked       (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonmesvolreservee_clicked        (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonretourresevol_clicked         (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonmeshotreserver_clicked        (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonafficherlisthot_clicked       (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonvalidervolres_clicked         (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonreshotdel_clicked             (GtkWidget      *button,
+                                        gpointer         user_data);
+void
+on_button16retour_clicked              (GtkWidget      *button,
+                                        gpointer         user_data);
+
+void
+on_button1800_clicked                  (GtkWidget      *button,
+                                        gpointer         user_data);
+
+
+void
+on_button14retour_clicked              (GtkWidget       *button,
+                                        gpointer         user_data);
+
+
+void
+on_button13retour_clicked              (GtkWidget     *button,
+                                        gpointer         user_data);
+
+void
+on_button11retour_clicked              (GtkWidget      *button,
+                                        gpointer         user_data);
+
+void
+on_buttonretour8_clicked               (GtkWidget       *button,
+                                        gpointer         user_data);
+
+
+void
+on_buttongestresvol_clicked            (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttongestreshot_clicked            (GtkWidget       *button,
+                                        gpointer         user_data);
+
+
+void
+on_buttonreshotdel_clicked             (GtkWidget       *button,
+                                        gpointer         user_data);
+
+
+void
+on_buttonsupprimervolres_clicked       (GtkWidget       *button,
+                                        gpointer         user_data);
+
+
+
+
+void
+on_buttonreservol_clicked              (GtkWidget       *button,
+                                        gpointer         user_data);
+
+
+
+void
+on_button1801_clicked                  (GtkWidget      *button,
+                                        gpointer         user_data);
