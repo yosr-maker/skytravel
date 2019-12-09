315e0e15a77cdb46bc8b9b52d1cc09d70044dbdc
yosr-maker <57325844+yosr-maker@users.noreply.github.com>
2019-12-09 08:43:33 +0100

    Add files via upload

---
+++ vol.h
@@ -0,0 +1,26 @@
+#include <gtk/gtk.h>
+
+typedef struct 
+{
+	char Ville_depart[40];
+	char Ville_arrivee[40];
+	int Jour;
+	int Mois;
+	int Annee;
+	char Duree_vol[40];
+	char Classe[40];
+	char Nbre_voyageurs[40];
+	char Prix_vol[40];
+	char Id_vol[40];
+	
+}Vol;
+
+
+
+void ajouter_vol(Vol p);
+void afficher_vol(GtkWidget *liste);
+void modifier(Vol p, Vol m);
+void afficher_hotelreservee(GtkWidget *liste);
+void afficher_hotel(GtkWidget *liste);
+void afficher_volreservee(GtkWidget *liste);
+
